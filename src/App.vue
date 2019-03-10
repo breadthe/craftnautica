089c42d75fd9d1c740c3ee4c96bf200edf4546bc
breadthe <ferdinand.bismarck@gmail.com>
2019-03-09 22:13:47 -0600

    Integrate Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav-main></nav-main>
+
     <router-view/>
   </div>
 </template>
 
+<script>
+import NavMain from '@/components/NavMain.vue';
+
+export default {
+  components: {
+    NavMain,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900');
+
+@tailwind preflight;
+@tailwind components;
+@tailwind utilities;
+
+@import './assets/index.scss';
 </style>
