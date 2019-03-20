63ac407e70fb5fc6abf2f552ba05e9e5b4b61cd0
breadthe <ferdinand.bismarck@gmail.com>
2019-03-19 20:35:38 -0500

    Starter items list

---
+++ src/items.js
@@ -0,0 +1,42 @@
+export default {
+  // ============= Mats.Minerals
+  Copper_Ore: null,
+  Cave_Sulfur: null,
+  Diamond: null,
+  Gas_Pod: null,
+  Gold: null,
+  Kyanite: null,
+  Lead: null,
+  Lithium: null,
+  Magnetite: null,
+  Metal_Salvage: null,
+  Nickel_Ore: null,
+  Quartz: null,
+  Ruby: null,
+  Salt_Deposit: null,
+  Alien_Feces: null,
+  Silver_Ore: null,
+  Stalker_Teeth: null,
+  Crystalline_Sulfur: null,
+  Uraninite_Crystal: null,
+
+  // ============= Mats.Corals
+  Coral_Tube_Sample: null,
+  Fungal_Sample: null,
+  Brain_Coral_Sample: null,
+  Table_Coral_Sample: null,
+
+  // ============= Mats.Flora
+  Acid_Mushroom: null,
+  Deep_Shroom: null,
+  Gel_Sack: null,
+
+  // ============= Recipes.Fabricator
+  Copper_Wire: [
+    { c: 'Copper_Ore', q: 2 },
+  ],
+  Battery: [
+    { c: 'Copper_Ore', q: 1 },
+    { c: 'Acid_Mushroom', q: 2 },
+  ],
+};
