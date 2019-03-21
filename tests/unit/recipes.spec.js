6bbea4085edf1e1af5d7b7eacea6b374b5d6d936
breadthe <ferdinand.bismarck@gmail.com>
2019-03-21 07:24:34 -0500

    Reorganize tests

--- tests/unit/recipes.spec.js
+++ tests/unit/recipes.spec.js
@@ -1,36 +1,30 @@
 import algo from '@/algo';
-// import { concat } from 'rxjs';
 
-test('it returns the correct list of base materials for any item', () => {
-  let res;
-
+describe('Test recipes', () => {
   /* Raw Materials */
-  res = algo.listOfMaterials('Copper_Ore');
-  expect(res).toEqual([{ c: 'Copper_Ore', q: 1 }]);
+  test('it returns the correct raw materials', () => {
+    expect(algo.listOfMaterials('Copper_Ore')).toEqual([{ c: 'Copper_Ore', q: 1 }]);
 
-  res = algo.listOfMaterials('Uraninite_Crystal');
-  expect(res).toEqual([{ c: 'Uraninite_Crystal', q: 1 }]);
+    expect(algo.listOfMaterials('Uraninite_Crystal')).toEqual([{ c: 'Uraninite_Crystal', q: 1 }]);
+  });
 
   /* Recipes.Fabricator.Electronics */
-  res = algo.listOfMaterials('Copper_Wire');
-  expect(res).toEqual([{ c: 'Copper_Ore', q: 2 }]);
+  test('it returns the correct list of base materials for Recipes.Fabricator.Electronics', () => {
+    expect(algo.listOfMaterials('Copper_Wire')).toEqual([{ c: 'Copper_Ore', q: 2 }]);
 
-  res = algo.listOfMaterials('Battery');
-  expect(res).toEqual([{ c: 'Copper_Ore', q: 1 }, { c: 'Acid_Mushroom', q: 2 }]);
+    expect(algo.listOfMaterials('Battery')).toEqual([{ c: 'Copper_Ore', q: 1 }, { c: 'Acid_Mushroom', q: 2 }]);
 
-  res = algo.listOfMaterials('Advanced_Wiring_Kit');
-  expect(res).toEqual([{ c: 'Silver_Ore', q: 2 }, { c: 'Gold', q: 3 }, { c: 'Table_Coral_Sample', q: 2 }, { c: 'Copper_Ore', q: 2 }]);
+    expect(algo.listOfMaterials('Advanced_Wiring_Kit')).toEqual([{ c: 'Silver_Ore', q: 2 }, { c: 'Gold', q: 3 }, { c: 'Table_Coral_Sample', q: 2 }, { c: 'Copper_Ore', q: 2 }]);
 
-  res = algo.listOfMaterials('Reactor_Rod');
-  expect(res).toEqual([{ c: 'Uraninite_Crystal', q: 3 }, { c: 'Lead', q: 1 }, { c: 'Titanium', q: 1 }, { c: 'Quartz', q: 2 }]);
+    expect(algo.listOfMaterials('Reactor_Rod')).toEqual([{ c: 'Uraninite_Crystal', q: 3 }, { c: 'Lead', q: 1 }, { c: 'Titanium', q: 1 }, { c: 'Quartz', q: 2 }]);
 
-  res = algo.listOfMaterials('Ion_Battery');
-  expect(res).toEqual([{ c: 'Ion_Cube', q: 1 }, { c: 'Gold', q: 1 }, { c: 'Silver_Ore', q: 1 }]);
+    expect(algo.listOfMaterials('Ion_Battery')).toEqual([{ c: 'Ion_Cube', q: 1 }, { c: 'Gold', q: 1 }, { c: 'Silver_Ore', q: 1 }]);
 
-  res = algo.listOfMaterials('Ion_Power_Cell');
-  expect(res).toEqual([{ c: 'Ion_Cube', q: 2 }, { c: 'Gold', q: 2 }, { c: 'Silver_Ore', q: 2 }, { c: 'Creepvine_Seed_Cluster', q: 0.5 }]);
+    expect(algo.listOfMaterials('Ion_Power_Cell')).toEqual([{ c: 'Ion_Cube', q: 2 }, { c: 'Gold', q: 2 }, { c: 'Silver_Ore', q: 2 }, { c: 'Creepvine_Seed_Cluster', q: 0.5 }]);
+  });
 
   /* Recipes.Fabricator.Deployables */
-  res = algo.listOfMaterials('Gravsphere');
-  expect(res).toEqual([{ c: 'Copper_Ore', q: 2 }, { c: 'Titanium', q: 1 }, { c: 'Acid_Mushroom', q: 2 }]);
+  test('it returns the correct list of base materials for Recipes.Fabricator.Deployables', () => {
+    expect(algo.listOfMaterials('Gravsphere')).toEqual([{ c: 'Copper_Ore', q: 2 }, { c: 'Titanium', q: 1 }, { c: 'Acid_Mushroom', q: 2 }]);
+  });
 });
