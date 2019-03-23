8310aaec2d147e830c3f4a303243c1fa56fe0951
breadthe <ferdinand.bismarck@gmail.com>
2019-03-23 09:16:37 -0500

    Add Subnautica & Below Zero links to main nav

--- src/components/NavMain.vue
+++ src/components/NavMain.vue
@@ -4,9 +4,11 @@
     class="bg-black p-4"
   >
     <div class="flex items-center justify-between max-w-lg mx-auto">
-      <router-link to="/" class="text-grey-light hover:text-white mr-2">Home</router-link>
-      <router-link to="/recipes" class="text-grey-light hover:text-white mr-2">Recipes</router-link>
-      <router-link to="/about" class="text-grey-light hover:text-white mr-2">About</router-link>
+      <router-link to="/" class="flex items-center justify-center w-12 h-12 p-2 rounded-full bg-grey-darkest text-sm hover:text-white mr-2">Home</router-link>
+      <router-link to="/sn" class="text-grey-light hover:text-white mr-2">Subnautica</router-link>
+      <router-link to="/bz" class="text-grey-light hover:text-white mr-2">Below Zero</router-link>
+      <!--<router-link to="/recipes" class="text-grey-light hover:text-white mr-2">Recipes</router-link>-->
+      <!--<router-link to="/about" class="text-grey-light hover:text-white mr-2">About</router-link>-->
 
       <search></search>
     </div>
