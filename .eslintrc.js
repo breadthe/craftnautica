6f120c0ce77e9d0c6653c4cc1c3201cda07981f5
breadthe <ferdinand.bismarck@gmail.com>
2019-03-23 07:57:11 -0500

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'linebreak-style': 'off',
+    'max-len': ['error', { 'code': 120 }],
+    // 'comma-dangle': ['error', 'always']
   },
   parserOptions: {
     parser: 'babel-eslint',
