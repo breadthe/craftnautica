f53d6f38821dcf98804c022b340c74eab9da395e
breadthe <ferdinand.bismarck@gmail.com>
2019-03-24 17:22:54 -0500

    Also search within item types

--- src/util.js
+++ src/util.js
@@ -1,15 +1,29 @@
 export default {
-  pretty: n => n.split('_').join(' '),
-  types: items => Object.values(items).map(v => v.t).filter((val, key, self) => self.indexOf(val) === key),
-  filterByType: (items, type) => Object.fromEntries(Object.entries(items).filter(i => i[1].t === type)),
-  search: (items, srcStr) => Object.fromEntries(Object.entries(items).filter(i => i[0].match(new RegExp(srcStr, 'ig')))),
+
+  pretty: n => n.split('_')
+    .join(' '),
+
+  types: items => Object.values(items)
+    .map(v => v.t)
+    .filter((val, key, self) => self.indexOf(val) === key),
+
+  filterByType: (items, type) => Object.fromEntries(Object.entries(items)
+    .filter(i => i[1].t === type)),
+
+  search: (items, srcStr) => Object.fromEntries(Object.entries(items)
+    .filter(i => i[0].match(new RegExp(srcStr, 'ig')) || i[1].t.match(new RegExp(srcStr, 'ig')))),
+
   fullDomainName: (domain) => {
     switch (domain) {
       case 'sn':
-      case 'subnautica': return 'Subnautica';
+      case 'subnautica':
+        return 'Subnautica';
       case 'bz':
-      case 'belowzero': return 'Below Zero';
-      default: return 'Subnautica';
+      case 'belowzero':
+        return 'Below Zero';
+      default:
+        return 'Subnautica';
     }
   },
+
 };
