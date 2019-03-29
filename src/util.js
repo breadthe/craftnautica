ce55878daf1981c400271bd07d587653d64ded21
breadthe <ferdinand.bismarck@gmail.com>
2019-03-28 21:33:40 -0500

    Improve search & refactor utils

--- src/util.js
+++ src/util.js
@@ -1,31 +1,31 @@
+const pretty = n => n.split('_')
+  .join(' ');
+
+const types = items => Object.values(items)
+  .map(v => v.t)
+  .filter((val, key, self) => self.indexOf(val) === key);
+
+const filterByType = (items, type) => Object.fromEntries(Object.entries(items)
+  .filter(i => i[1].t === type));
+
+const formatType = type => type.split('.').join(' > ').split('_').join(' ');
+
+const search = (items, srcStr) => Object.fromEntries(Object.entries(items)
+  .filter(i => formatType(i[0]).match(new RegExp(srcStr, 'ig')) || formatType(i[1].t).match(new RegExp(srcStr, 'ig'))));
+
+const fullDomainName = (domain) => {
+  switch (domain) {
+    case 'sn':
+    case 'subnautica':
+      return 'Subnautica';
+    case 'bz':
+    case 'belowzero':
+      return 'Below Zero';
+    default:
+      return 'Subnautica';
+  }
+};
+
 export default {
-
-  pretty: n => n.split('_')
-    .join(' '),
-
-  types: items => Object.values(items)
-    .map(v => v.t)
-    .filter((val, key, self) => self.indexOf(val) === key),
-
-  filterByType: (items, type) => Object.fromEntries(Object.entries(items)
-    .filter(i => i[1].t === type)),
-
-  search: (items, srcStr) => Object.fromEntries(Object.entries(items)
-    .filter(i => i[0].match(new RegExp(srcStr, 'ig')) || i[1].t.match(new RegExp(srcStr, 'ig')))),
-
-  fullDomainName: (domain) => {
-    switch (domain) {
-      case 'sn':
-      case 'subnautica':
-        return 'Subnautica';
-      case 'bz':
-      case 'belowzero':
-        return 'Below Zero';
-      default:
-        return 'Subnautica';
-    }
-  },
-
-  formatType: type => type.split('.').join(' > ').split('_').join(' '),
-
+  pretty, types, filterByType, search, fullDomainName, formatType,
 };
