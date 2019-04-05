3bc3c899be9e0eb7af15029d2b64f5fb27b389a8
breadthe <ferdinand.bismarck@gmail.com>
2019-04-04 21:09:53 -0500

    click-outside directive

--- src/main.js
+++ src/main.js
@@ -6,6 +6,23 @@ import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
+// TODO: Extract this
+Vue.directive('click-outside', {
+  bind: function (el, binding, vnode) {
+    el.clickOutsideEvent = (event) => {
+      // here I check that click was outside the el and his childrens
+      if (!(el === event.target || el.contains(event.target))) {
+        // and if it did, call method provided in attribute value
+        vnode.context[binding.expression](event);
+      }
+    };
+    document.body.addEventListener('click', el.clickOutsideEvent);
+  },
+  unbind: function (el) {
+    document.body.removeEventListener('click', el.clickOutsideEvent);
+  },
+});
+
 new Vue({
   router,
   store,
