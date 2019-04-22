7f9418b6e836a4182ee292cbd4abb67949e0d53f
breadthe <ferdinand.bismarck@gmail.com>
2019-04-21 19:33:14 -0500

    Fix Netlify form submit

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,10 +6,13 @@
 
     <form
         name="contact"
-        method="post"
-        data-netlify="true"
+        method="POST"
         class="mt-8"
+        data-netlify="true"
+        netlify-honeypot="honeybadger"
     >
+      <input type="text" name="honeybadger" />
+
       <div
           class="flex justify-start pb-4"
       >
@@ -17,6 +20,7 @@
           <input placeholder="Your Name" type="text" id="name" name="name" class="p-2" />
         </div>
       </div>
+
       <div
           class="flex justify-start pb-4"
       >
@@ -24,6 +28,7 @@
           <input placeholder="Your Email" type="email" id="email" name="email" class="p-2" />
         </div>
       </div>
+
       <div
           class="flex justify-start pb-4"
       >
@@ -31,6 +36,7 @@
           <textarea placeholder="Message" id="message" name="message" class="p-2 w-64 h-32"></textarea>
         </div>
       </div>
+
       <div class="text-right">
         <button
             type="submit"
