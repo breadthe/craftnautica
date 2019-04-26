1e93b7ab3a21eef44a0c26c74f988334cbfa7126
breadthe <ferdinand.bismarck@gmail.com>
2019-04-25 22:09:11 -0500

    Extract directives

--- src/main.js
+++ src/main.js
@@ -1,41 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
+import directives from './directives';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
-// TODO: Extract this
-Vue.directive('click-outside', {
-  bind: function (el, binding, vnode) {
-    el.clickOutsideEvent = (event) => {
-      // here I check that click was outside the el and his childrens
-      if (!(el === event.target || el.contains(event.target))) {
-        // and if it did, call method provided in attribute value
-        vnode.context[binding.expression](event);
-      }
-    };
-    document.body.addEventListener('click', el.clickOutsideEvent);
-  },
-  unbind: function (el) {
-    document.body.removeEventListener('click', el.clickOutsideEvent);
-  },
-});
-
-// Use v-focus to focus an input
-Vue.directive('focus', {
-  inserted(el) {
-    el.focus();
-  },
-});
-
-// Use v-select to select all the text in an input
-Vue.directive('select', {
-  inserted(el) {
-    el.select();
-  },
-});
+Vue.directive('click-outside', directives.clickOutside);
+Vue.directive('focus', directives.focus);
+Vue.directive('select', directives.select);
 
 new Vue({
   router,
